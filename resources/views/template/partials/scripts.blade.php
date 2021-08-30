165c4c8f478ce468718fa8d060f8dd5d686878a1
rahmanramsi <rahmanramsi19@gmail.com>
2021-08-30 14:45:06 +0800

    update

--- resources/views/template/partials/scripts.blade.php
+++ resources/views/template/partials/scripts.blade.php
@@ -1,14 +1,3 @@
-<!-- Tabler Core -->
-<script src="{{ asset('vendor/core/js/tabler.min.js') }}"></script>
-<!-- Libs JS -->
-<script src="{{ asset('vendor/core/libs/jquery/dist/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/core/libs/jquery-form/jquery.form.min.js') }}"></script>
-<script src="{{ asset('vendor/core/libs/jquery-validate/jquery.validate.min.js') }}"></script>
-
-@livewireScripts
-
-{!! Core::renderScript() !!}
-
 <script>
     @if (config('core.path'))
         const baseUrl = "{{ url('/') . '/' . config('core.path') }}";
@@ -17,32 +6,17 @@
     @endif
 
     const adminUrl = baseUrl + '/admin';
-
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    // Enable pusher logging - don't include this in production
-
-    @auth
-        @if (config('app.debug'))
-            // Pusher.logToConsole = false;
-        @endif
-        var pusher = new Pusher('{{ config('broadcasting.connections.pusher.key') }}', {
-        cluster: '{{ config('broadcasting.connections.pusher.options.cluster') }}'
-        });
-    @endauth
-
-    // Example of a channel subscription
-    // var channel = pusher.subscribe('new-customer');
-    // channel.bind('OpenJournalTeam\\Master\\Events\\NewCustomer', function(data) {
-    //     Toast.fire({
-    //         icon: 'info',
-    //         title: 'New Customer',
-    //         text: `New Customer ${data.customer.name}`
-    //     })
-    // });
 </script>
+
+<!-- Tabler Core -->
+<script src="{{ asset('vendor/core/js/tabler.min.js') }}"></script>
+<!-- Libs JS -->
+<script src="{{ asset('vendor/core/libs/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/core/libs/jquery-form/jquery.form.min.js') }}"></script>
+<script src="{{ asset('vendor/core/libs/jquery-validate/jquery.validate.min.js') }}"></script>
+@livewireScripts
+
+{!! Core::renderScript() !!}
+
+
 @yield('scripts')
