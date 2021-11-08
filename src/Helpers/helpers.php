6d7410443935f61b76431482919de40f77de555d
rahmanramsi <rahmanramsi19@gmail.com>
2021-11-08 08:43:43 +0800

    change default status code to 422

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -66,7 +66,7 @@ if (!function_exists('response_success')) {
 }
 
 if (!function_exists('response_error')) {
-    function response_error($error, $status_code = 500)
+    function response_error($error, $status_code = 422)
     {
         return response()->json([
             'success' => false,
