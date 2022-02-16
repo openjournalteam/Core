ad3437a4b3d9a467137db67d3a2c7ec1d6f52860
thisnugroho <this.nugroho@gmail.com>
2022-02-16 10:36:04 +0800

    Fix migrate error

--- database/migrations/2021_11_06_070127_add_users_columns_table.php
+++ database/migrations/2021_11_06_070127_add_users_columns_table.php
@@ -14,8 +14,7 @@ class AddUsersColumnsTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->unique()->after('email');
-            $table->integer('status')->default(0);
+            $table->string('username')->unique()->after('email')->nullable();
         });
     }
 
