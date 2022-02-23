098115e33016c4bf5d639d586ef38053cb9bb498
thisnugroho <this.nugroho@gmail.com>
2022-02-23 16:35:38 +0800

    add apexchart

--- config/config.php
+++ config/config.php
@@ -181,6 +181,10 @@ return [
       'href' => 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
       'async' => true,
     ],
+    [
+      'href' => 'vendor/core/libs/apexcharts/dist/apexcharts.min.js',
+      'async' => true
+    ]
   ]
 
 ];
