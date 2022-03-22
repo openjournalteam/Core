eff8bccb3e1dd3241fadeeb349a308ba050a5ecb
thisnugroho <this.nugroho@gmail.com>
2022-03-22 14:44:55 +0800

    Fix padding footer

--- resources/views/template/partials/footer.blade.php
+++ resources/views/template/partials/footer.blade.php
@@ -1,16 +1,18 @@
 {{ apply_filters('core::template::partials::footer::before', '') }}
 
-<footer class="footer footer-transparent d-print-none">
+<footer class="footer footer-transparent d-print-none p-0">
     <div class="container-fluid">
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ms-lg-auto">
 
             </div>
-            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+            <div class="col-12 col-lg-auto mt-4 mt-lg-0">
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; 2021
-                        <a href="https://openjournaltheme.com" class="link-secondary">Open Journal Team</a>.
+                        <a href="https://openjournaltheme.com" class="link-secondary">
+                            <span style="color:#F31675;">Open Journal</span> <span style="color:black;">Team</span>
+                        </a>.
                         All rights reserved.
                     </li>
                 </ul>
