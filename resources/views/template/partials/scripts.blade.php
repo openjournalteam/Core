30ff02b57efe6fea3817b10e36d460d0332d2310
thisnugroho <this.nugroho@gmail.com>
2022-06-04 14:49:12 +0800

    remove secret things

--- resources/views/template/partials/scripts.blade.php
+++ resources/views/template/partials/scripts.blade.php
@@ -18,20 +18,6 @@
 <script src="{{ asset('vendor/core/libs/jquery-validate/jquery.validate.min.js') }}"></script>
 <script src="{{ asset('vendor/core/libs/pusher/pusher.min.js') }}"></script>
 
-<script>
-  // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
-
-    var pusher = new Pusher('b7d9914eb73974f7c387', {
-      cluster: 'ap1'
-    });
-
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-      alert(JSON.stringify(data));
-    });
-</script>
-
 @livewireScripts
 <x-livewire-alert::scripts />
 
