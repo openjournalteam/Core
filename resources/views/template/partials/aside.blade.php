e209bfaaeb69f6f1f3c61b8361fe525216f2c73b
thisnugroho <this.nugroho@gmail.com>
2022-06-08 08:31:17 +0800

    replace with hosting logo

--- resources/views/template/partials/aside.blade.php
+++ resources/views/template/partials/aside.blade.php
@@ -5,7 +5,7 @@
         </button>
         <h1 class="navbar-brand navbar-brand-autodark">
             <a href="{{ route('core.home') }}">
-                <img src="{{ asset('vendor/core/img/logo_ojt.png') }}" width="110" height="32" alt="OpenJournalTeam Panel" class="navbar-brand-image" style="filter: drop-shadow(0 0 0.75rem crimson);">
+                <img src="{{ asset('vendor/core/img/logo_ojt_hosting.png') }}" width="200" height="32" alt="OJT Hosting Logo" class="navbar-brand-image" style="height:2.3rem !important;">
             </a>
         </h1>
         <div class="navbar-nav flex-row d-lg-none">
